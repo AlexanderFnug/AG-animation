5b7820ad2854c7045810f1b6ab8ed62d48493ee3
Alexander <alexander.fnug-soltani@1508.dk>
2024-06-04 16:03:04 +0200

    two boyz

--- script.js
+++ script.js
@@ -1,77 +1,83 @@
-const density = '${MB} ';
-
-let buffer;
-let asciiDiv;
-let radii = [];
-let circleIndex = 0;
-let maxCircles = 30;
-let maxRadius = 400;
-let growthRate = 2;
-let delayBetweenCircles = 20;
-let frameCountDelay = 0;
-
-function setup() {
-  noCanvas();
-  buffer = createGraphics(window.innerWidth, window.innerHeight);
-  buffer.angleMode(DEGREES);
-  asciiDiv = createDiv();
-  asciiDiv.id("ascii-art");
-
-  for (let i = 0; i < maxCircles; i++) {
-    radii.push(0);
-  }
-}
-
-function draw() {
-  buffer.background(0);
-  buffer.noFill();
-  buffer.stroke(255);
-  buffer.strokeWeight(4);
-
-  buffer.push();
-  buffer.translate(buffer.width / 4, buffer.height / 8);
-
-  for (let i = 0; i < maxCircles; i++) {
-    buffer.ellipse(0, 0, radii[i], radii[i]);
-  }
-
-  buffer.pop();
-
-  frameCountDelay++;
-  if (frameCountDelay >= delayBetweenCircles) {
-    if (circleIndex < maxCircles - 1) {
-      circleIndex++;
+const sketch1 = (p) => {
+    const density = '${MB} ';
+  
+    let buffer;
+    let asciiDiv;
+    let radii = [];
+    let circleIndex = 0;
+    let maxCircles = 30;
+    let maxRadius = 400;
+    let growthRate = 2;
+    let delayBetweenCircles = 20;
+    let frameCountDelay = 0;
+  
+    p.setup = () => {
+      p.noCanvas();
+      buffer = p.createGraphics(p.windowWidth, p.windowHeight);
+      buffer.angleMode(p.DEGREES);
+      buffer.clear(); // Clear the buffer to make it transparent
+      asciiDiv = p.createDiv();
+      asciiDiv.id("ascii-art");
+  
+      for (let i = 0; i < maxCircles; i++) {
+        radii.push(0);
+      }
     }
-    frameCountDelay = 0;
-  }
-
-  for (let i = 0; i <= circleIndex; i++) {
-    if (radii[i] < maxRadius) {
-      radii[i] += growthRate;
+  
+    p.draw = () => {
+      buffer.clear(); // Clear the buffer to maintain transparency
+      buffer.noFill();
+      buffer.stroke(255);
+      buffer.strokeWeight(4);
+  
+      buffer.push();
+      buffer.translate(buffer.width / 4, buffer.height / 8);
+  
+      for (let i = 0; i < maxCircles; i++) {
+        buffer.ellipse(0, 0, radii[i], radii[i]);
+      }
+  
+      buffer.pop();
+  
+      frameCountDelay++;
+      if (frameCountDelay >= delayBetweenCircles) {
+        if (circleIndex < maxCircles - 1) {
+          circleIndex++;
+        }
+        frameCountDelay = 0;
+      }
+  
+      for (let i = 0; i <= circleIndex; i++) {
+        if (radii[i] < maxRadius) {
+          radii[i] += growthRate;
+        }
+      }
+  
+      if (circleIndex == maxCircles - 1 && radii[circleIndex] >= maxRadius) {
+        radii = radii.map(() => 0);
+        circleIndex = 0;
+      }
+  
+      // Generate the ASCII art
+      buffer.loadPixels();
+      let asciiImage = "";
+      for (let j = 0; j < buffer.height; j += 8) {
+        for (let i = 0; i < buffer.width; i += 2) {
+          const pixelIndex = 4 * (i + j * buffer.width);
+          const r = buffer.pixels[pixelIndex + 0];
+          const g = buffer.pixels[pixelIndex + 1];
+          const b = buffer.pixels[pixelIndex + 2];
+          const avg = (r + g + b) / 3;
+          const len = density.length;
+          const charIndex = Math.floor(p.map(avg, 0, 255, len - 1, 0));
+          const c = density.charAt(charIndex);
+          asciiImage += c === " " ? "&nbsp;" : c;
+        }
+        asciiImage += "<br/>";
+      }
+      asciiDiv.html(asciiImage);
     }
-  }
-
-  if (circleIndex == maxCircles - 1 && radii[circleIndex] >= maxRadius) {
-    radii = radii.map(() => 0);
-    circleIndex = 0;
-  }
-
-  // Generate the ASCII art
-  buffer.loadPixels();
-  let asciiImage = "";
-  for (let j = 0; j < buffer.height; j += 8) {
-    for (let i = 0; i < buffer.width; i += 2) {
-      const pixelIndex = 4 * (i + j * buffer.width);
-      const r = buffer.pixels[pixelIndex + 0];
-      const g = buffer.pixels[pixelIndex + 1];
-      const b = buffer.pixels[pixelIndex + 2];
-      const avg = (r + g + b) / 3;
-      const len = density.length;
-      const charIndex = floor(map(avg, 0, 255, len - 1, 0));
-      const c = density.charAt(charIndex);
-      asciiImage += c === " " ? "&nbsp;" : c;
-    }
-    asciiImage += "<br/>";
-  }
-  asciiDiv.html(asciiImage);
-}
+  };
+  
+  new p5(sketch1);
+  
--- script2.js
+++ script2.js
@@ -1,49 +1,83 @@
-//const density = '       .:-i|=+%O#@';
-const density = "Ñ@#W$9876543210?!abc;:+=-,._                    ";
-
-let buffer;
-let asciiDiv;
-let angle = 0;
-
-function setup() {
-  noCanvas();
-  buffer = createGraphics(window.width, window.height);
-  buffer.angleMode(DEGREES);
-  asciiDiv = createDiv();
-}
-
-function draw() {
-   buffer.background(255);
-  buffer.push();
-  buffer.translate(buffer.width / 2, buffer.height / 2);
-  buffer.rotate(angle);
-  buffer.noFill();
-  buffer.stroke(0);
-  buffer.strokeWeight(2);
-  buffer.beginShape();
-  buffer.vertex(-40, -40);
-  buffer.circle(50, 50, 25);
-  buffer.endShape(CLOSE);
-  buffer.pop();
-  angle += 0.5;
-
-  // Use the buffer as the image for the ASCII art
-  buffer.loadPixels();
-  let asciiImage = "";
-  for (let j = 0; j < buffer.height; j++) {
-    for (let i = 0; i < buffer.width; i++) {
-      const pixelIndex = (i + j * buffer.width) * 4;
-      const r = buffer.pixels[pixelIndex + 0];
-      const g = buffer.pixels[pixelIndex + 1];
-      const b = buffer.pixels[pixelIndex + 2];
-      const avg = (r + g + b) / 3;
-      const len = density.length;
-      const charIndex = floor(map(avg, 0, 255, 0, len));
-      const c = density.charAt(charIndex);
-      if (c == " ") asciiImage += "&nbsp;";
-      else asciiImage += c;
+const sketch2 = (p) => {
+    const density2 = '${MB} ';
+  
+    let buffer2;
+    let asciiDiv2;
+    let radii2 = [];
+    let circleIndex2 = 0;
+    let maxCircles2 = 30;
+    let maxRadius2 = 500;
+    let growthRate2 = 2;
+    let delayBetweenCircles2 = 20;
+    let frameCountDelay2 = 0;
+  
+    p.setup = () => {
+      p.noCanvas();
+      buffer2 = p.createGraphics(p.windowWidth, p.windowHeight);
+      buffer2.angleMode(p.DEGREES);
+      buffer2.clear(); // Clear the buffer to make it transparent
+      asciiDiv2 = p.createDiv();
+      asciiDiv2.id("ascii-art2");
+  
+      for (let i = 0; i < maxCircles2; i++) {
+        radii2.push(0);
+      }
     }
-    asciiImage += "<br/>";
-  }
-  asciiDiv.html(asciiImage);
-}
+  
+    p.draw = () => {
+      buffer2.clear(); // Clear the buffer to maintain transparency
+      buffer2.noFill();
+      buffer2.stroke(255);
+      buffer2.strokeWeight(4);
+  
+      buffer2.push();
+      buffer2.translate(buffer2.width / 4, buffer2.height / 8);
+  
+      for (let i = 0; i < maxCircles2; i++) {
+        buffer2.ellipse(0, 0, radii2[i], radii2[i]);
+      }
+  
+      buffer2.pop();
+  
+      frameCountDelay2++;
+      if (frameCountDelay2 >= delayBetweenCircles2) {
+        if (circleIndex2 < maxCircles2 - 1) {
+          circleIndex2++;
+        }
+        frameCountDelay2 = 0;
+      }
+  
+      for (let i = 0; i <= circleIndex2; i++) {
+        if (radii2[i] < maxRadius2) {
+          radii2[i] += growthRate2;
+        }
+      }
+  
+      if (circleIndex2 == maxCircles2 - 1 && radii2[circleIndex2] >= maxRadius2) {
+        radii2 = radii2.map(() => 0);
+        circleIndex2 = 0;
+      }
+  
+      // Generate the ASCII art
+      buffer2.loadPixels();
+      let asciiImage2 = "";
+      for (let j = 0; j < buffer2.height; j += 8) {
+        for (let i = 0; i < buffer2.width; i += 2) {
+          const pixelIndex2 = 4 * (i + j * buffer2.width);
+          const r = buffer2.pixels[pixelIndex2 + 0];
+          const g = buffer2.pixels[pixelIndex2 + 1];
+          const b = buffer2.pixels[pixelIndex2 + 2];
+          const avg2 = (r + g + b) / 3;
+          const len2 = density2.length;
+          const charIndex2 = Math.floor(p.map(avg2, 0, 255, len2 - 1, 0));
+          const c = density2.charAt(charIndex2);
+          asciiImage2 += c === " " ? "&nbsp;" : c;
+        }
+        asciiImage2 += "<br/>";
+      }
+      asciiDiv2.html(asciiImage2);
+    }
+  };
+  
+  new p5(sketch2);
+  

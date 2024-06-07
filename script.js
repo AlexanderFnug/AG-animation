57c75c64abd8140fa0e3c197504adabf2e9dbad3
Alexander <alexander.fnug-soltani@1508.dk>
2024-06-07 11:38:03 +0200

    Fix script2.js reference in index.html and adjust animation timing in style.css

--- script.js
+++ script.js
@@ -1,91 +1,103 @@
-const sketch1 = (p) => {
-    const density = 'M  ';
-  
-    let buffer;
-    let asciiDiv;
-    let radii = [];
-    let heights = [];
-    let circleIndex = 0;
-    let maxCircles = 30;
-    let maxRadius = 400;
-    let growthRate = 2;
-    let heightGrowthRate // Adjust this to change the vertical growth rate
-    let delayBetweenCircles = 5;
-    let frameCountDelay = 0;
-  
-    p.setup = () => {
-      p.noCanvas();
-      buffer = p.createGraphics(p.windowWidth, p.windowHeight);
-      buffer.angleMode(p.DEGREES);
-      buffer.clear(); // Clear the buffer to make it transparent
-      asciiDiv = p.createDiv();
-      asciiDiv.id("ascii-art");
-  
-      for (let i = 0; i < maxCircles; i++) {
-        radii.push(0);
-        heights.push(0);
-        
-      }
+document.addEventListener('DOMContentLoaded', () => {
+    const word1 = "MB";
+    const word2 = "KG";
+    const density2 = `${word1} ${word2}.`;
+    
+    let canvas2, ctx2;
+    let radii2 = [];
+    let heights2 = [];
+    let circleIndex2 = 0;
+    const maxCircles2 = 200;
+    const maxRadius2 = 800;
+    const growthRate2 = 0.3;
+    const delayBetweenCircles2 = 5;
+    let frameCountDelay2 = 0;
+    let heightGrowthRate2;
+
+    const asciiDiv2 = document.getElementById('ascii-art');
+
+    function setup() {
+        canvas2 = document.createElement('canvas');
+        canvas2.width = window.innerWidth/4;
+        canvas2.height = window.innerHeight;
+        ctx2 = canvas2.getContext('2d');
+        //document.body.appendChild(canvas2);
+
+        for (let i = 0; i < maxCircles2; i++) {
+            radii2.push(0);
+            heights2.push(0);
+        }
+
+        draw();
     }
-  
-    p.draw = () => {
-      buffer.clear(); // Clear the buffer to maintain transparency
-      buffer.noFill();
-      buffer.stroke(255);
-      buffer.strokeWeight(1);
-  
-      buffer.push();
-      buffer.translate(buffer.width / 4, buffer.height / 8);
-  
-      for (let i = 0; i < maxCircles; i++) {
-        buffer.ellipse(0, 0, radii[i], heights[i]);
-        buffer.rotate(-360 / maxCircles);
-        heightGrowthRate = i/8 + 1;
-      }
-  
-      buffer.pop();
-  
-      frameCountDelay++;
-      if (frameCountDelay >= delayBetweenCircles) {
-        if (circleIndex < maxCircles - 1) {
-          circleIndex++;
+
+    function draw() {
+        ctx2.clearRect(0, 0, canvas2.width, canvas2.height);
+        ctx2.strokeStyle = 'white';
+        ctx2.lineWidth = 1;
+        ctx2.save();
+        ctx2.translate(canvas2.width / 4, canvas2.height / 8);
+
+        for (let i = 0; i < maxCircles2; i++) {
+            ctx2.beginPath();
+            ctx2.ellipse(0, 0, radii2[i], heights2[i], 0, 0, 2 * Math.PI);
+            ctx2.stroke();
+            ctx2.rotate(2 * Math.PI / maxCircles2);
+            heightGrowthRate2 = i / 64;
         }
-        frameCountDelay = 0;
-      }
-  
-      for (let i = 0; i <= circleIndex; i++) {
-        if (radii[i] < maxRadius) {
-          radii[i] += growthRate;
-          heights[i] += heightGrowthRate;
+
+        ctx2.restore();
+
+        frameCountDelay2++;
+        if (frameCountDelay2 >= delayBetweenCircles2) {
+            if (circleIndex2 < maxCircles2 - 1) {
+                circleIndex2++;
+            }
+            frameCountDelay2 = 0;
         }
-      }
-  
-      if (circleIndex == maxCircles - 1 && radii[circleIndex] >= maxRadius) {
-        radii = radii.map(() => 0);
-        heights = heights.map(() => 0);
-        circleIndex = 0;
-      }
-  
-      // Generate the ASCII art
-      buffer.loadPixels();
-      let asciiImage = "";
-      for (let j = 0; j < buffer.height; j += 8) {
-        for (let i = 0; i < buffer.width; i += 2) {
-          const pixelIndex = 4 * (i + j * buffer.width);
-          const r = buffer.pixels[pixelIndex + 0];
-          const g = buffer.pixels[pixelIndex + 1];
-          const b = buffer.pixels[pixelIndex + 2];
-          const avg = (r + g + b) / 3;
-          const len = density.length;
-          const charIndex = Math.floor(p.map(avg, 0, 255, len - 1, 0));
-          const c = density.charAt(charIndex);
-          asciiImage += c === " " ? "&nbsp;" : c;
+
+        for (let i = 0; i <= circleIndex2; i++) {
+            if (radii2[i] < maxRadius2) {
+                radii2[i] += growthRate2;
+                heights2[i] += heightGrowthRate2;
+            }
         }
-        asciiImage += "<br/>";
-      }
-      asciiDiv.html(asciiImage);
+
+        if (circleIndex2 == maxCircles2 - 1 && radii2[circleIndex2] >= maxRadius2) {
+            radii2 = radii2.map(() => 0);
+            heights2 = heights2.map(() => 0);
+            circleIndex2 = 0;
+        }
+
+        generateAsciiArt2();
+        requestAnimationFrame(draw);
     }
-  };
-  
-  new p5(sketch1);
-  
+
+    function generateAsciiArt2() {
+        const imageData2 = ctx2.getImageData(0, 0, canvas2.width, canvas2.height);
+        const pixels2 = imageData2.data;
+        let asciiImage2 = '';
+
+        for (let j = 0; j < canvas2.height; j += 2) {
+            for (let i = 0; i < canvas2.width; i += 2) {
+                const pixelIndex2 = 4 * (i + j * canvas2.width);
+                const r2 = pixels2[pixelIndex2];
+                const g2 = pixels2[pixelIndex2 + 1];
+                const b2 = pixels2[pixelIndex2 + 2];
+                const avg2 = (r2 + g2 + b2) / 3;
+                const len2 = density2.length;
+                const charIndex2 = Math.floor(map(avg2, 0, 255, len2 - 1, 0));
+                const c2 = density2.charAt(charIndex2);
+                asciiImage2 += c2 === " " ? "&nbsp;" : c2;
+            }
+            asciiImage2 += "<br/>";
+        }
+        asciiDiv2.innerHTML = asciiImage2;
+    }
+
+    function map(value, start1, stop1, start2, stop2) {
+        return ((value - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
+    }
+
+    setup();
+});
--- script2.js
+++ script2.js
@@ -1,91 +1,102 @@
-const sketch2 = (p) => {
-    let word1 = "MB"
-    let word2 = "KG"
-    const density2 = `B${word1} ${word2} `;
-  
-    let buffer2;
-    let asciiDiv2;
+document.addEventListener('DOMContentLoaded', () => {
+    const word1 = "MB";
+    const word2 = "KG";
+    const density2 = `${word1} ${word2}.`;
+    
+    let canvas2, ctx2;
     let radii2 = [];
     let heights2 = [];
     let circleIndex2 = 0;
-    let maxCircles2 = 30;
-    let maxRadius2 = 400;
-    let growthRate2 = 2;
-    let heightGrowthRate2; // Adjust this to change the vertical growth rate
-    let delayBetweenCircles2 = 5;
+    const maxCircles2 = 30;
+    const maxRadius2 = 800;
+    const growthRate2 = 0.5;
+    const delayBetweenCircles2 = 5;
     let frameCountDelay2 = 0;
-  
-    p.setup = () => {
-      p.noCanvas();
-      buffer2 = p.createGraphics(p.windowWidth, p.windowHeight);
-      buffer2.angleMode(p.DEGREES);
-      buffer2.clear(); // Clear the buffer to make it transparent
-      asciiDiv2 = p.createDiv();
-      asciiDiv2.id("ascii-art2");
-  
-      for (let i = 0; i < maxCircles2; i++) {
-        radii2.push(0);
-        heights2.push(0);
-      }
+    let heightGrowthRate2;
+
+    const asciiDiv2 = document.getElementById('ascii-art2');
+
+    function setup() {
+        canvas2 = document.createElement('canvas');
+        canvas2.width = window.innerWidth;
+        canvas2.height = window.innerHeight;
+        ctx2 = canvas2.getContext('2d');
+
+        for (let i = 0; i < maxCircles2; i++) {
+            radii2.push(0);
+            heights2.push(0);
+        }
+
+        draw();
     }
-  
-    p.draw = () => {
-      buffer2.clear(); // Clear the buffer to maintain transparency
-      buffer2.noFill();
-      buffer2.stroke(255);
-      buffer2.strokeWeight(1);
-  
-      buffer2.push();
-      buffer2.translate(buffer2.width / 4, buffer2.height / 8);
-  
-      for (let i = 0; i < maxCircles2; i++) {
-        buffer2.ellipse(0, 0, radii2[i], heights2[i]);
-        buffer2.rotate(360 / maxCircles2);
-        heightGrowthRate2 = i / 8 + 1;
-      }
-  
-      buffer2.pop();
-  
-      frameCountDelay2++;
-      if (frameCountDelay2 >= delayBetweenCircles2) {
-        if (circleIndex2 < maxCircles2 - 1) {
-          circleIndex2++;
+
+    function draw() {
+        ctx2.clearRect(0, 0, canvas2.width, canvas2.height);
+        ctx2.strokeStyle = 'white';
+        ctx2.lineWidth = 1;
+        ctx2.save();
+        ctx2.translate(canvas2.width / 2, canvas2.height / 2);
+
+        for (let i = 0; i < maxCircles2; i++) {
+            ctx2.beginPath();
+            ctx2.ellipse(0, 0, radii2[i], heights2[i], 0, 0, 2 * Math.PI);
+            ctx2.stroke();
+            ctx2.rotate(2 * Math.PI / maxCircles2);
+            heightGrowthRate2 = i / 8 + 1;
         }
-        frameCountDelay2 = 0;
-      }
-  
-      for (let i = 0; i <= circleIndex2; i++) {
-        if (radii2[i] < maxRadius2) {
-          radii2[i] += growthRate2;
-          heights2[i] += heightGrowthRate2;
+
+        ctx2.restore();
+
+        frameCountDelay2++;
+        if (frameCountDelay2 >= delayBetweenCircles2) {
+            if (circleIndex2 < maxCircles2 - 1) {
+                circleIndex2++;
+            }
+            frameCountDelay2 = 0;
         }
-      }
-  
-      if (circleIndex2 == maxCircles2 - 1 && radii2[circleIndex2] >= maxRadius2) {
-        radii2 = radii2.map(() => 0);
-        heights2 = heights2.map(() => 0);
-        circleIndex2 = 0;
-      }
-  
-      // Generate the ASCII art
-      buffer2.loadPixels();
-      let asciiImage2 = "";
-      for (let j = 0; j < buffer2.height; j += 8) {
-        for (let i = 0; i < buffer2.width; i += 2) {
-          const pixelIndex2 = 4 * (i + j * buffer2.width);
-          const r = buffer2.pixels[pixelIndex2 + 0];
-          const g = buffer2.pixels[pixelIndex2 + 1];
-          const b = buffer2.pixels[pixelIndex2 + 2];
-          const avg2 = (r + g + b) / 3;
-          const len2 = density2.length;
-          const charIndex2 = Math.floor(p.map(avg2, 0, 255, len2 - 1, 0));
-          const c = density2.charAt(charIndex2);
-          asciiImage2 += c === " " ? "&nbsp;" : c;
+
+        for (let i = 0; i <= circleIndex2; i++) {
+            if (radii2[i] < maxRadius2) {
+                radii2[i] += growthRate2;
+                heights2[i] += heightGrowthRate2;
+            }
         }
-        asciiImage2 += "<br/>";
-      }
-      asciiDiv2.html(asciiImage2);
+
+        if (circleIndex2 == maxCircles2 - 1 && radii2[circleIndex2] >= maxRadius2) {
+            radii2 = radii2.map(() => 0);
+            heights2 = heights2.map(() => 0);
+            circleIndex2 = 0;
+        }
+
+        generateAsciiArt2();
+        requestAnimationFrame(draw);
     }
-  };
-  
-  new p5(sketch2);
+
+    function generateAsciiArt2() {
+        const imageData2 = ctx2.getImageData(0, 0, canvas2.width, canvas2.height);
+        const pixels2 = imageData2.data;
+        let asciiImage2 = '';
+
+        for (let j = 0; j < canvas2.height; j += 2) {
+            for (let i = 0; i < canvas2.width; i += 4) {
+                const pixelIndex2 = 4 * (i + j * canvas2.width);
+                const r2 = pixels2[pixelIndex2];
+                const g2 = pixels2[pixelIndex2 + 1];
+                const b2 = pixels2[pixelIndex2 + 2];
+                const avg2 = (r2 + g2 + b2) / 3;
+                const len2 = density2.length;
+                const charIndex2 = Math.floor(map(avg2, 0, 255, len2 - 1, 0));
+                const c2 = density2.charAt(charIndex2);
+                asciiImage2 += c2 === " " ? "&nbsp;" : c2;
+            }
+            asciiImage2 += "<br/>";
+        }
+        asciiDiv2.innerHTML = asciiImage2;
+    }
+
+    function map(value, start1, stop1, start2, stop2) {
+        return ((value - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
+    }
+
+    setup();
+});
